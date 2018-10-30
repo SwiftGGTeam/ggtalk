c08774431ced9f9173539081ee35d6cb85fb63bd
Jie Liang <lj925184928@gmail.com>
2018-10-30 18:43:46 -0500

    build

---
+++ build/precache-manifest.a456f493ff72e0f4b58de9ae42b27724.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0849a5e79712b10e1531925e3edb879",
+    "url": "/static/media/mejs-controls.f0849a5e.svg"
+  },
+  {
+    "revision": "9b4a63a8e8710a91d43276ec8dd1222c",
+    "url": "/static/media/mediaelement-flash-video.9b4a63a8.swf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d34082a4331c4393b520",
+    "url": "/static/js/main.d34082a4.chunk.js"
+  },
+  {
+    "revision": "0a7cd74e2b48835027e1",
+    "url": "/static/js/1.0a7cd74e.chunk.js"
+  },
+  {
+    "revision": "d34082a4331c4393b520",
+    "url": "/static/css/main.a59305d5.chunk.css"
+  },
+  {
+    "revision": "0a7cd74e2b48835027e1",
+    "url": "/static/css/1.52c8bf14.chunk.css"
+  },
+  {
+    "revision": "d518d7384bb30aed2d7ff45341623dcc",
+    "url": "/index.html"
+  }
+];
